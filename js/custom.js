af3c0a5bf88742e66dca4785bbdc44d68c5ffd99
franklin-raph <igboekwulusifranklin@gmail.com>
2023-05-26 15:26:33 +0100

    implemented the contact form and waitlist

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,10 @@
 const subscribeEmail = document.querySelector(".input-subscribe");
 const subscribeBtn = document.querySelector(".subscribe-btn");
 
+const contactName = document.querySelector("#name");
+const contactEmail = document.querySelector("#email");
+const contactMessage = document.querySelector("#message");
+
 subscribeBtn.addEventListener("click", async () => {
   if (subscribeEmail.value.length <= 1) {
     alert("Please provide an email address");
@@ -8,12 +12,36 @@ subscribeBtn.addEventListener("click", async () => {
     const response = await fetch("https://sportbetpredict.onrender.com/api/visitor/add/waitlist", {
       method: "POST",
       headers: {
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
       },
-      body: { email: subscribeEmail.value },
+      body: JSON.stringify({ email: subscribeEmail.value }),
     });
     const data = await response.json();
+    if (response) {
+      alert(data.message);
+      setTimeout(() => {
+        subscribeEmail.value = "";
+      }, 3000);
+    }
+  }
+});
 
-    console.log(data);
+document.querySelector("#contactForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const response = await fetch("https://sportbetpredict.onrender.com/api/visitor/contact-us", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: contactName.value, email: contactEmail.value, message: contactMessage.value }),
+  });
+  const data = await response.json();
+  if (response) {
+    alert(data.message);
+    setTimeout(() => {
+      contactName.value = "";
+      contactEmail.value = "";
+      contactMessage.value = "";
+    }, 3000);
   }
 });
